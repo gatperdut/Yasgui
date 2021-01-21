ba04dbd4c1fd2a1ada37ef16467687a0d6143488
carlosr <carlos.romero@onlim.com>
2021-01-21 11:41:50 +0100

    whatever

---
+++ build/ts/packages/yasr/src/plugins/boolean/index.d.ts
@@ -0,0 +1,12 @@
+import Yasr from "../../";
+import { Plugin } from "../";
+export interface PluginConfig {}
+export default class Boolean implements Plugin<PluginConfig> {
+  private yasr;
+  priority: number;
+  hideFromSelection: boolean;
+  constructor(yasr: Yasr);
+  draw(): void;
+  canHandleResults(): boolean;
+  getIcon(): HTMLElement;
+}
---
+++ build/ts/packages/yasr/src/plugins/error/index.d.ts
@@ -0,0 +1,14 @@
+import { Plugin } from "../";
+import Yasr from "../../";
+export default class Error implements Plugin<never> {
+  private yasr;
+  options: never;
+  constructor(yasr: Yasr);
+  canHandleResults(): boolean;
+  private getTryBtn;
+  private getCorsMessage;
+  draw(): Promise<void>;
+  getIcon(): HTMLElement;
+  priority: number;
+  hideFromSelection: boolean;
+}
